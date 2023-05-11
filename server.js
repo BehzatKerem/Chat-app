b61c827909c8697e2bab572ce6cd1e35d23586c4
Behzat Kerem Dursun <62844191+BehzatKerem@users.noreply.github.com>
2023-05-12 00:02:47 +0300

    Update

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // Import the Express and Socket.IO modules
 const express = require('express');
 const socket = require('socket.io');
+const port = 8080;
 
 // Create a new Express app
 const app = express();
 // Start the Express server on port 3000 and log a message to the console
+<<<<<<< Updated upstream
 const server = app.listen(3000, () => {
     console.log('Server started. Port: 3000');
+=======
+const server = app.listen(port, () => {
+    console.log("Sunucu başlatıldı. Port: }");
+>>>>>>> Stashed changes
 });
 
 // Serve static files from the 'public' directory
