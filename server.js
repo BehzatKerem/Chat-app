6120ca610717d2504f019df1c35affe1e238f071
Behzat Kerem Dursun <behzat_dursun@outlook.com>
2023-05-12 00:03:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,9 @@ const port = 8080;
 // Create a new Express app
 const app = express();
 // Start the Express server on port 3000 and log a message to the console
-<<<<<<< Updated upstream
 const server = app.listen(3000, () => {
     console.log('Server started. Port: 3000');
-=======
-const server = app.listen(port, () => {
-    console.log("Sunucu başlatıldı. Port: }");
->>>>>>> Stashed changes
-});
+}
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
